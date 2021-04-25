4c3732203fd3628349fc40e35f9430f86b527f85
ErickFocil <edfocilg@hotmail.com>
2021-04-25 12:19:17 -0700

    Arreglos

---
+++ CatalogoTest/View/ProductoCell.swift
@@ -0,0 +1,24 @@
+//
+//  ProductoCell.swift
+//  CatalogoTest
+//
+//  Created by user185916 on 4/25/21.
+//  Copyright © 2021 user185916. All rights reserved.
+//
+
+import UIKit
+
+class ProductoCell: UITableViewCell {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+
+}
---
+++ CatalogoTest/View/RowView.swift
@@ -0,0 +1,21 @@
+//
+//  RowView.swift
+//  CatalogoTest
+//
+//  Created by user185916 on 4/25/21.
+//  Copyright © 2021 user185916. All rights reserved.
+//
+
+import SwiftUI
+
+struct RowView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+struct RowView_Previews: PreviewProvider {
+    static var previews: some View {
+        RowView()
+    }
+}
